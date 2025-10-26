c4b009ae019b07b5d01dc11c98d89498b9daa158
seungjun <jjw5741@naver.com>
2025-10-27 04:47:45 +0900

    chore/#4: 필요없는 import 제거

--- app/src/main/java/com/sopt/dive/core/designsystem/component/DiveBasicTextField.kt
+++ app/src/main/java/com/sopt/dive/core/designsystem/component/DiveBasicTextField.kt
@@ -2,7 +2,6 @@ package com.sopt.dive.core.designsystem.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
