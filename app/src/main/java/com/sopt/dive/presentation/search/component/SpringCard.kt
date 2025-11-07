8bb25ec06d2d1aa654ef91bea00de2dad836305c
seungjun <jjw5741@naver.com>
2025-11-07 11:58:12 +0900

    feat/#6: 스프링 카드 애니메이션 수정

--- app/src/main/java/com/sopt/dive/presentation/search/component/SpringCard.kt
+++ app/src/main/java/com/sopt/dive/presentation/search/component/SpringCard.kt
@@ -1,14 +1,11 @@
 package com.sopt.dive.presentation.search.component
 
-import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
@@ -27,6 +24,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.sopt.dive.R
 import com.sopt.dive.core.util.noRippleClickable
 
@@ -34,158 +32,123 @@ import com.sopt.dive.core.util.noRippleClickable
 fun SpringCard(
     modifier: Modifier = Modifier,
 ) {
-    var isCardFlipped by remember { mutableStateOf(false) }
+    var isCardClicked by remember { mutableStateOf(false) }
 
     // Figma Spring (Mass=1, Stiffness=177.8, Damping≈20)
-    val springSpec = spring<Float>(
-        stiffness = 177.8f, dampingRatio = 0.85f
+    val springSpecFloat = spring<Float>(
+        stiffness = 177.8f, dampingRatio = 0.75f
     )
+
+    val springSpecDp = spring<Dp>(
+        stiffness = 177.8f, dampingRatio = 0.75f
+    )
+
     val density = LocalDensity.current
 
-    val transition = updateTransition(targetState = isCardFlipped, label = "flip")
+    val transition = updateTransition(targetState = isCardClicked, label = "card-transition")
 
     val rotateCardY by transition.animateFloat(
-        transitionSpec = {
-            tween(
-                durationMillis = 1000,
-                easing = FastOutSlowInEasing,
-            )
-        },
-        label = "rotateCardY",
-    ) { flipped -> if (flipped) 180f else 0f }
+        transitionSpec = { springSpecFloat },
+        label = "rotate-card-y",
+    ) { isClicked -> if (isClicked) 180f else 0f }
 
     val move by transition.animateFloat(
-        transitionSpec = { springSpec }, label = "move"
-    ) { state -> if (state) with(density) { 56.dp.toPx() } else 0f }
+        transitionSpec = { springSpecFloat },
+        label = "move",
+    ) { isClicked -> if (isClicked) with(density) { 56.dp.toPx() } else 0f }
 
     val frontElevation by transition.animateDp(
-        transitionSpec = { tween(250, easing = FastOutSlowInEasing) }, label = "front-elev"
-    ) { flipped ->
-        if (!flipped) 8.dp else 0.dp
+        transitionSpec = { springSpecDp },
+        label = "front-elev",
+    ) { isClicked ->
+        if (!isClicked) 10.dp else 0.dp
     }
 
     val backElevation by transition.animateDp(
-        transitionSpec = { tween(250, easing = FastOutSlowInEasing) }, label = "back-elev"
-    ) { flipped ->
-        if (flipped) 8.dp else 0.dp
+        transitionSpec = { springSpecDp },
+        label = "back-elev",
+    ) { isClicked ->
+        if (isClicked) 5.dp else 0.dp
     }
 
     val backTextAlpha by transition.animateFloat(
-        transitionSpec = { tween(1000, easing = FastOutSlowInEasing) }, label = "back-alpha"
-    ) { flipped ->
-        if (flipped) 1f else 0f
+        transitionSpec = { springSpecFloat },
+        label = "back-alpha",
+    ) { isClicked ->
+        if (isClicked) 1f else 0f
     }
 
+    val frontShape = RoundedCornerShape(
+        topStart = 20.dp,
+        topEnd = 80.dp,
+        bottomStart = 80.dp,
+        bottomEnd = 20.dp,
+    )
+
+    val backShape = RoundedCornerShape(
+        topStart = 80.dp,
+        topEnd = 20.dp,
+        bottomStart = 20.dp,
+        bottomEnd = 80.dp,
+    )
+
     Box(
-        modifier = modifier.noRippleClickable { isCardFlipped = !isCardFlipped },
+        modifier = modifier.noRippleClickable { isCardClicked = !isCardClicked },
         contentAlignment = Alignment.Center,
     ) {
-        if (isCardFlipped) {
-            BackCard(
-                textAlpha = backTextAlpha,
-                backElevation = backElevation,
-                frontElevation = frontElevation,
-                modifier = Modifier.graphicsLayer {
-                    rotationY = rotateCardY - 180f
+        Surface(
+            modifier = Modifier
+                .zIndex(if (isCardClicked) 1f else 0f),
+            shape = backShape,
+            shadowElevation = backElevation,
+        ) {
+            BackCard(textAlpha = backTextAlpha)
+        }
+
+        Surface(
+            modifier = Modifier
+                .zIndex(if (isCardClicked) 0f else 1f)
+                .graphicsLayer {
+                    rotationY = rotateCardY
                     cameraDistance = 12f * this.density
                     translationX = move
                     translationY = move
                 },
-            )
-        } else {
-            FrontCard(
-                textAlpha = backTextAlpha,
-                backElevation = backElevation,
-                frontElevation = frontElevation,
-                modifier = Modifier.graphicsLayer {
-                    rotationY = rotateCardY
-                    cameraDistance = 12f * this.density
-                },
+            shape = frontShape,
+            shadowElevation = frontElevation,
+        ) {
+            Image(
+                painter = painterResource(R.drawable.img_front_card),
+                contentDescription = null,
             )
         }
     }
 }
 
 @Composable
-private fun BoxScope.FrontCard(
+private fun BackCard(
     textAlpha: Float,
-    backElevation: Dp,
-    frontElevation: Dp,
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
 ) {
-    Surface(
-        shape = RoundedCornerShape(
-            topStart = 80.dp, topEnd = 20.dp, bottomStart = 20.dp, bottomEnd = 80.dp
-        ),
-        shadowElevation = backElevation,
-        modifier = Modifier.wrapContentSize(),
+    Box(
+        modifier = modifier,
     ) {
         Image(
             painter = painterResource(id = R.drawable.img_card_background),
             contentDescription = null,
         )
+
         Box(
-            modifier = Modifier.matchParentSize(),
+            modifier = Modifier
+                .matchParentSize(),
         ) {
             Text(
-                text = "침착맨 팝업 가고싶다".repeat(50),
+                text = "침착맨팝업가고싶다".repeat(50),
                 color = Color.White,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.graphicsLayer { alpha = textAlpha },
-            )
-        }
-    }
-
-    Surface(
-        shape = RoundedCornerShape(
-            topStart = 20.dp, topEnd = 80.dp, bottomStart = 80.dp, bottomEnd = 20.dp
-        ), shadowElevation = frontElevation, modifier = modifier.wrapContentSize()
-    ) {
-        Image(
-            painter = painterResource(R.drawable.img_front_card), contentDescription = null
-        )
-    }
-}
-
-@Composable
-private fun BoxScope.BackCard(
-    textAlpha: Float,
-    backElevation: Dp,
-    frontElevation: Dp,
-    modifier: Modifier,
-) {
-    Surface(
-        shape = RoundedCornerShape(
-            topStart = 80.dp, topEnd = 20.dp, bottomStart = 20.dp, bottomEnd = 80.dp
-        ),
-        shadowElevation = frontElevation,
-        modifier = modifier.wrapContentSize(),
-    ) {
-        Image(
-            painter = painterResource(R.drawable.img_front_card),
-            contentDescription = null,
-        )
-    }
-
-    Surface(
-        shape = RoundedCornerShape(
-            topStart = 80.dp, topEnd = 20.dp, bottomStart = 20.dp, bottomEnd = 80.dp
-        ),
-        shadowElevation = backElevation,
-        modifier = Modifier.wrapContentSize(),
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.img_card_background),
-            contentDescription = null,
-        )
-        Box(
-            modifier = Modifier.matchParentSize(),
-        ) {
-            Text(
-                text = "침착맨 팝업 가고싶다".repeat(50),
-                color = Color.White,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.graphicsLayer { alpha = textAlpha },
+                modifier = Modifier
+                    .align(alignment = Alignment.Center)
+                    .graphicsLayer { alpha = textAlpha },
             )
         }
     }
