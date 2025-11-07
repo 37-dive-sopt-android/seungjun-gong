c76aa86d05e55679e5b918e9bbfc6bccde665bf2
seungjun <jjw5741@naver.com>
2025-11-07 11:58:39 +0900

    chore/#6: import 제거

--- app/src/main/java/com/sopt/dive/presentation/search/component/SpringCard.kt
+++ app/src/main/java/com/sopt/dive/presentation/search/component/SpringCard.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
